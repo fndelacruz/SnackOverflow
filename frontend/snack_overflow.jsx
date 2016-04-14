4e6670377b61f35a457dd599e4e90788afccda53
Florante Dela Cruz <fndelacruz@gmail.com>
2016-04-14 11:42:31 -0700

    create basic navbar

--- frontend/snack_overflow.jsx
+++ frontend/snack_overflow.jsx
@@ -1,21 +1,31 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var NavBar = require('./components/nav_bar');
 
-var MyApp = React.createClass({
-  render: function() {
-    return (
-      <div>MyApp</div>
-    );
-  }
-});
+var App = (
+  <Router>
+    <Route path='/' component={NavBar}>
+    </Route>
+  </Router>
+);
+
+// IDEAL GOAL
+// var App = (
+//   <Router>
+//     <Route path='/' component={NavBar}>
+//       <IndexRoute component={Home}/>
+//       <Route path='questions/' component={QuestionsIndex} />
+//       <Route path='tags/' component={TagsIndex}/>
+//     </Route>
+//   </Router>
+// );
 
 document.addEventListener('DOMContentLoaded', function() {
   var root = document.getElementById('root');
   if (root) {
-    ReactDOM.render(
-      React.createElement(MyApp),
-      document.getElementById('root')
-    );
+    ReactDOM.render(App, document.getElementById('root'));
   }
   console.log('loaded');
 });
