effc7120d25a91eb974ed9438e6a4ae2a63a487b
Florante Dela Cruz <fndelacruz@gmail.com>
2016-04-29 21:07:16 -0700

    created find_with_reputation_and_tags by merging with_reputation and with_tags

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,7 +59,7 @@ class User < ActiveRecord::Base
   has_many :received_question_votes, through: :questions, source: :votes
 
   def self.find_with_reputation(id)
-    user = User.find_by_sql [<<-SQL, userId: id]
+    user = User.find_by_sql [<<-SQL, user_id: id]
       SELECT
         users.*,
         (
@@ -88,7 +88,7 @@ class User < ActiveRecord::Base
           LEFT JOIN
             votes ON (questions.id = votes.votable_id AND votes.votable_type = 'Question')
           WHERE
-            users.id = :userId
+            users.id = :user_id
           GROUP BY
             users.id
           ORDER BY
@@ -111,7 +111,7 @@ class User < ActiveRecord::Base
           LEFT JOIN
             votes ON (answers.id = votes.votable_id AND votes.votable_type = 'Answer')
           WHERE
-            users.id = :userId
+            users.id = :user_id
           GROUP BY
             users.id
           ORDER BY
@@ -134,7 +134,7 @@ class User < ActiveRecord::Base
           LEFT JOIN
             votes ON (comments.id = votes.votable_id AND votes.votable_type = 'Comment')
           WHERE
-            users.id = :userId
+            users.id = :user_id
           GROUP BY
             users.id
           ORDER BY
@@ -149,7 +149,7 @@ class User < ActiveRecord::Base
           JOIN
             votes ON users.id = votes.user_id
           WHERE
-            users.id = :userId AND
+            users.id = :user_id AND
             votes.votable_type = 'Answer' AND votes.value = -1
            GROUP BY
             users.id
@@ -186,6 +186,129 @@ class User < ActiveRecord::Base
       .first
   end
 
+  def self.find_with_reputation_and_tags(id)
+    user_tags = User.find_by_sql [<<-SQL, user_id: id]
+      SELECT
+        users.*,
+        (
+          COALESCE(sq1.question_vote_reputation, 0) +
+          COALESCE(sq2.answer_vote_reputation, 0) +
+          COALESCE(sq3.comment_vote_reputation, 0) +
+          COALESCE(sq4.given_answer_downvote_reputation)
+        ) AS sql_reputation,
+        tags_q.name,
+        COUNT(tags_q)
+      FROM
+        users
+      JOIN
+        (
+          SELECT
+            users.id,
+            users.display_name,
+            SUM(
+              CASE votes.value
+              WHEN 1 THEN 10
+              WHEN -1 THEN -4
+              END
+            ) AS question_vote_reputation
+          FROM
+            users
+          LEFT JOIN
+            questions ON users.id = questions.user_id
+          LEFT JOIN
+            votes ON (questions.id = votes.votable_id AND votes.votable_type = 'Question')
+          WHERE
+            users.id = :user_id
+          GROUP BY
+            users.id
+          ORDER BY
+            users.id
+        ) AS sq1 on sq1.id = users.id
+      JOIN
+        (
+          SELECT
+            users.id,
+            SUM(
+              CASE votes.value
+              WHEN 1 THEN 20
+              WHEN -1 THEN -4
+              END
+            ) AS answer_vote_reputation
+          FROM
+            users
+          LEFT JOIN
+            answers ON users.id = answers.user_id
+          LEFT JOIN
+            votes ON (answers.id = votes.votable_id AND votes.votable_type = 'Answer')
+          WHERE
+            users.id = :user_id
+          GROUP BY
+            users.id
+          ORDER BY
+            users.id
+        ) AS sq2 ON sq1.id = users.id
+      JOIN
+        (
+          SELECT
+            users.id,
+            SUM(
+              CASE votes.value
+              WHEN 1 THEN 1
+              WHEN -1 THEN -1
+              END
+            ) AS comment_vote_reputation
+          FROM
+            users
+          LEFT JOIN
+            comments ON users.id = comments.user_id
+          LEFT JOIN
+            votes ON (comments.id = votes.votable_id AND votes.votable_type = 'Comment')
+          WHERE
+            users.id = :user_id
+          GROUP BY
+            users.id
+          ORDER BY
+            users.id
+        ) AS sq3 ON sq2.id = users.id
+      JOIN
+        (
+          SELECT
+            users.id, COUNT(votes.*) * -2 AS given_answer_downvote_reputation
+          FROM
+            users
+          JOIN
+            votes ON users.id = votes.user_id
+          WHERE
+            users.id = :user_id AND
+            votes.votable_type = 'Answer' AND votes.value = -1
+           GROUP BY
+            users.id
+          ORDER BY
+            users.id
+        ) AS sq4 ON sq1.id = users.id
+      LEFT JOIN
+        questions ON users.id = questions.user_id
+      LEFT JOIN
+        taggings AS taggings_q ON questions.id = taggings_q.question_id
+      LEFT JOIN
+        tags AS tags_q ON taggings_q.tag_id = tags_q.id
+      WHERE
+        users.id = :user_id
+      GROUP BY
+        users.id,
+        tags_q.name,
+        sq1.question_vote_reputation,
+        sq2.answer_vote_reputation,
+        sq3.comment_vote_reputation,
+        sq4.given_answer_downvote_reputation
+      ORDER BY
+        users.id, COUNT(tags_q) DESC, tags_q.name
+    SQL
+    user_tags[0].tags = {}
+    user_tags.each { |tag| user_tags[0].tags[tag.name] = tag.count if tag.name }
+      .first
+  end
+
   def self.find_by_credentials(email, password)
     user = User.find_by_email(email)
     user if user && user.is_password?(password)
@@ -205,7 +328,7 @@ class User < ActiveRecord::Base
       .all
   end
 
-  def self.show_find(userId)
+  def self.show_find(user_id)
     User.includes(
         { questions: :votes },
         { given_answers: [:votes, :question] },
@@ -217,7 +340,7 @@ class User < ActiveRecord::Base
         :question_tags,
         received_answer_votes: { votable: :associated_tags },
         received_question_votes: { votable: :associated_tags })
-      .find(userId)
+      .find(user_id)
   end
 
   def associated_tags_score
