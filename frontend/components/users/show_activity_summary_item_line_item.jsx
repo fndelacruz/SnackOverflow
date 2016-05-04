4d0d2672b8ea3babbde069ddc1259a61e5b78bbc
Florante Dela Cruz <fndelacruz@gmail.com>
2016-05-04 16:48:21 -0700

    truncate titles in ShowActivitySummaryItemLineItem. remove guide outlines

--- frontend/components/users/show_activity_summary_item_line_item.jsx
+++ frontend/components/users/show_activity_summary_item_line_item.jsx
@@ -1,4 +1,12 @@
 var React = require('react');
+function handleTitleTruncate(title) {
+  if (title.length > 70) {
+    return title.slice(0, 70) + '...';
+  } else {
+    return title;
+  }
+}
+
 var ShowActivitySummaryItemLineItem = React.createClass({
   render: function() {
     return (
@@ -9,9 +17,8 @@ var ShowActivitySummaryItemLineItem = React.createClass({
         <div
           onClick={this.props.handleClick}
           className='show-activity-summary-item-line-item-title link'>
-          {this.props.title}
+          {handleTitleTruncate(this.props.title)}
         </div>
-        {this.props.createdAt.toLocaleString()}
       </div>
     );
   }
