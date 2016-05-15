65728de9d259f135d88cb958f2a0cc79c4ddd017
Florante Dela Cruz <fndelacruz@gmail.com>
2016-05-15 11:47:26 -0700

    add helper content to QuestionFormSidebar

--- frontend/components/questions/form_sidebar.jsx
+++ frontend/components/questions/form_sidebar.jsx
@@ -3,11 +3,11 @@ var React = require('react');
 function renderHeader(focus) {
   switch (focus) {
     case 'title':
-      return 'Make a good title. Do these things.';
+      return 'How to ask.';
     case 'content':
-      return 'No bad content allowed. Be specific';
+      return 'Be specific.';
     case 'tags':
-      return 'Tags are good in moderation.';
+      return 'How to tag.';
   }
 }
 
@@ -16,25 +16,27 @@ function renderBody(focus) {
     case 'title':
       return (
         <ul className='question-form-double-sidebar-content'>
-          <li>title</li>
-          <li>title</li>
-          <li>title</li>
+          <li>• Please make sure your question is relevant to food, snacks, cooking or some culinary concept.</li>
+          <li>• Before asking a question, do a quick search above to see if your question has already been answered.</li>
+          <li>• Title is 120 characters maximum.</li>
         </ul>
       );
     case 'content':
       return (
         <ul className='question-form-double-sidebar-content'>
-          <li>content</li>
-          <li>content</li>
-          <li>content</li>
+          <li>• Include any relevant details that could help other users better answer your question.</li>
+          <li>• Keep your question content relevant to the title provided. Please create a new question for related, but tangential topics.</li>
+          <li>• Main question content is 700 characters maximum. </li>
         </ul>
       );
     case 'tags':
       return (
         <ul className='question-form-double-sidebar-content'>
-          <li>tags</li>
-          <li>tags</li>
-          <li>tags</li>
+          <li>• Tags are keywords that help group similar questions together.</li>
+          <li>• Prefer to use established tags over creating new tags.</li>
+          <li>• To initiate tag creation while tag-searching, press the enter key.</li>
+          <li>• Permitted tag characters are [ a-z 0-9 - ]</li>
+          <li>• 5 tags maximum.</li>
         </ul>
       );
   }
